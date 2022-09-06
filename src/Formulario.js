ae6d75c9ebc0e865edc55dabdf8e0b07d5a0149a
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-05 21:11:25 -0300

    picker

--- src/Formulario.js
+++ src/Formulario.js
@@ -1,6 +1,7 @@
 import { View, Text, TextInput } from 'react-native';
 import React from 'react';
 import styles from '../styles/Formulario.jsx';
+import { Picker } from '@react-native-picker/picker'
 const Formulario = () => {
   return (
     <>
@@ -12,8 +13,17 @@ const Formulario = () => {
         </View>
       <Text>Formulario</Text>
     </View>
-    <View>
-
+    <View style={{ width: '100%', }}>
+        <Picker>
+            <Picker.Item label='--Seleccione un pais--' value=''/>
+            <Picker.Item label='Estados Unidos' value='US'/>
+            <Picker.Item label='Mexico' value='MX'/>
+            <Picker.Item label='Argentina' value='AR'/>
+            <Picker.Item label='Colombia' value='CO'/>
+            <Picker.Item label='Costa Rica' value='CR'/>
+            <Picker.Item label='España' value='ES'/>
+            <Picker.Item label='Peru' value='PE'/>
+        </Picker>
     </View>
     
     </>
